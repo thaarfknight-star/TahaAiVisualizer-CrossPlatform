6fb6f532980ea53c86ffe0e80929de2c4bd793ae
thaarfknight-star <thaarfknight@gmail.com>
2026-09-14 17:22:13 +0330

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.tahdigi.visualizer',
-  appName: 'TahaAi Visualizer',
-  webDir: 'www'
-};
-
-export default config;
